7eb7e3ffd3d308ca8e823271ea213498505b458e
Klow-CodeThink <keiji.low@computhink.com.sg>
2025-04-24 08:31:29 +0000

    Apr 24, 2025, 8:31 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,12 +1,6 @@
 # print("Hello from lesson 7")
 
-thesum = 0
 
-for i in range(5):
-    num = int(input("What is number " + str(i + 1) + "? " ))
-    thesum = thesum + num
-
-print("The sum of the numbers is " + str(thesum) + ".")
 
 
 
