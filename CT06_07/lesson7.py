cacfe40ac981b2417595a659dbc34f2c813c51ca
Klow-CodeThink <keiji.low@computhink.com.sg>
2025-04-24 08:47:50 +0000

    Apr 24, 2025, 8:47 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -5,7 +5,7 @@ num_stu = int(input("Enter the number of students: "))
 thesum = 0
 
 for i in range(1, num_stu + 1):
-    score = int(input("What is the score for Student " + str(i+1) + " ? "))
+    score = int(input("What is the score for Student " + str(i) + " ? "))
     thesum = thesum + score
 
 print(f"The average score is ({thesum / num_stu}.")
