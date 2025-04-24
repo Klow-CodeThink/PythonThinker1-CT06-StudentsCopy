be39af3b381f1c7d08d17a3727864d9b96ab7032
Klow-CodeThink <keiji.low@computhink.com.sg>
2025-04-24 03:52:47 +0000

    Apr 24, 2025, 3:52 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -1,33 +1,4 @@
-# print("Hello from lesson 2")
-
-# Example 1
-
-# min_age = 12
-# user_age = int(input("Enter your age: "))
-# if user_age < min_age:
-#     print("Access denied.")
-# else:
-#     print("Access granted.")
-
-# Example 2
-
-# num1 = int(input("Enter the first number: "))
-# num2 = int(input("Enter the second number: "))
-# ans = num1 + num2
-# print(ans)
-
-# Example 3
-
-# age = int(input("Enter your age: "))
-# print(age)
-
-# Example 4
-
-# apple = 2
-# num_apple = 10
-# total_price = apple * num_apple
-# print(total_price)
-
+print("Hello from lesson 2")
 
 
 ######## Write the pseudocode in comments for task 2 here
