9fc82b08dfb6f7805aeb9a0cbc9c04b49d2edce4
Klow-CodeThink <keiji.low@computhink.com.sg>
2025-04-24 03:37:03 +0000

    Apr 24, 2025, 3:37 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -16,7 +16,9 @@
 
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
-ans = num
+ans = num1 + num2
+print(ans)
+
 
 ######## Write the pseudocode in comments for task 2 here
 # Using comments, translate the code shown on screen into pseudocode.
