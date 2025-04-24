3a33f5c736b8d0f74361b1af185a0f6899b82c76
Klow-CodeThink <keiji.low@computhink.com.sg>
2025-04-24 08:17:47 +0000

    Apr 24, 2025, 8:17 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -4,7 +4,8 @@ user_name = input("Enter your name: ")
 n = int(input("Enter a number: "))
 
 for i in range(n):
-    print("Nice to meet you " + u)
+    print("Nice to meet you " + user_name)
+
 
 
 
