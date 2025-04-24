8cb1297200271de797e2cf6805fce8350227ec74
Klow-CodeThink <keiji.low@computhink.com.sg>
2025-04-24 07:00:43 +0000

    Apr 24, 2025, 7:00 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,18 +1,1 @@
-# print("Hello from lesson 5")
-
-# user_name = input("What is the birthday person's name? ")
-# user_age = int(input("What is the age he/she is turning this year? "))
-# user_msg = input("Type a personal message for the birthday boy/girl: ")
-
-# print(f"Happy {user_age}th birthday, {user_name}! {user_msg}")
-
-# for i in range (10):
-#     print("Snow!")
-#     print("Ball!")
-
-
-thesum = 0
-
-for i in range(10):
-    thesum = thesum + 1
-print(thesum)
+print("Hello from lesson 5")
