a8192ba72098ced7a29570ecaa9ea12debad4ecd
Klow-CodeThink <keiji.low@computhink.com.sg>
2025-04-24 08:50:09 +0000

    Apr 24, 2025, 8:50 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,14 +1,5 @@
 # print("Hello from lesson 7")
 
-num_stu = int(input("Enter the number of students: "))
-
-thesum = 0
-
-for i in range(1, num_stu + 1):
-    score = int(input("What is the score for Student " + str(i) + " ? "))
-    thesum = thesum + score
-
-print(f"The average score is {thesum / num_stu}.")
 
 
 
