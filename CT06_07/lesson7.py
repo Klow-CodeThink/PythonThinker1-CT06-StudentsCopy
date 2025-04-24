8d2ec1e8e55757228673158dcc5fd227e22f9701
Klow-CodeThink <keiji.low@computhink.com.sg>
2025-04-24 08:26:53 +0000

    Apr 24, 2025, 8:26 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -3,9 +3,10 @@
 thesum = 0
 
 for i in range(5):
-    num = input("What is number " + str(i) + "?" )
-    print(num)
-    
+    num = input("What is number " + str(i + 1) + "?" )
+
+print(num)
+
 
 
 
