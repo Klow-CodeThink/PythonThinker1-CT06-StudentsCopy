c701c70d4dbaefc18963c9e6895d0e42357fe58b
Klow-CodeThink <keiji.low@computhink.com.sg>
2025-04-24 04:48:49 +0000

    Apr 24, 2025, 4:48 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,7 +1,7 @@
 # print("Hello from lesson 5")
 
 user_name = input("What is the birthday person's name? ")
-user_age = input("What is the age he/she is turning this year? ")
+user_age = int(input("What is the age he/she is turning this year? "))
 user_msg = input("Type a personal message for the birthday boy/girl: ")
 
 print()
