541ea296cda2254994d10d3e725e7517a0a4e338
Klow-CodeThink <keiji.low@computhink.com.sg>
2025-04-24 08:33:54 +0000

    Apr 24, 2025, 8:33 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,6 +1,6 @@
 # print("Hello from lesson 7")
 
-num = int(input("What number for the timestable? "))
+num = int(input("What number for the timestable0? "))
 
 for i in range(1, 13):
     print(f"{num} x {i} = {num * i}")
