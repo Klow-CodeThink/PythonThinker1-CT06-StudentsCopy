8561f83e2c159febb3c7d1eb16c06fb7e3e6ba9d
Klow-CodeThink <keiji.low@computhink.com.sg>
2025-04-24 08:45:56 +0000

    Apr 24, 2025, 8:45 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -4,7 +4,7 @@ num_stu = int(input("Enter the number of students: "))
 
 thesum = 0
 
-for i in range(5):
+for i in range(1, ):
     score = int(input("What is the score for Student " + str(i+1) + " ? "))
     thesum = thesum + score
 
