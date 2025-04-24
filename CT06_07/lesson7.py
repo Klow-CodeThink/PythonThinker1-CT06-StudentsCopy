b027cd5bf3162d9758ea03dfac4fca9c1f5b83e8
Klow-CodeThink <keiji.low@computhink.com.sg>
2025-04-24 08:16:03 +0000

    Apr 24, 2025, 8:16 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,7 +1,7 @@
 # print("Hello from lesson 7")
 
 user_word = input("Enter a word: ")
-num = input("Enter a number: ")
+num = int(input("Enter a number: "))
 
 for i in range(num):
     print(user_word)
