0e04ee655dfd492106940a39667f6f1e46fb13e0
Klow-CodeThink <keiji.low@computhink.com.sg>
2025-04-24 08:03:15 +0000

    Apr 24, 2025, 8:03 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,18 +1,1 @@
 print("Hello from lesson 5")
-
-# Ask for the total number of students
-num_students = int(input("Enter the number of students: "))
-
-# Initialize a total variable to store the sum of marks
-total_marks = 0
-
-# Use a for loop to ask for each student's mark
-for i in range(1, num_students + 1):
-    mark = float(input("Enter mark for student " + str(i) + ": "))
-    total_marks += mark  # Add the mark to the total
-
-# Calculate the average
-average = total_marks / num_students
-
-# Display the average
-print("The average mark is:", round(average, 2))
