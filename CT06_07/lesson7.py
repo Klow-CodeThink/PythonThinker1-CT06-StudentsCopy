0ceef653b3e3b999d330df3dccb41b14bf9aca53
Klow-CodeThink <keiji.low@computhink.com.sg>
2025-04-24 08:29:20 +0000

    Apr 24, 2025, 8:29 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -3,7 +3,7 @@
 thesum = 0
 
 for i in range(5):
-    num = input("What is number " + str(i + 1) + "? " )
+    num = int(input("What is number " + str(i + 1) + "? " ))
     thesum = thesum + num
 
 print(num)
