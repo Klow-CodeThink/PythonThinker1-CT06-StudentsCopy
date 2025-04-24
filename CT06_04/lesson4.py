33d744b5438b68cc09afc065b75c2b5489f2127f
Klow-CodeThink <keiji.low@computhink.com.sg>
2025-04-24 04:10:07 +0000

    Apr 24, 2025, 4:10 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,8 +1,10 @@
 # print("Hello from lesson 4")
 
-red = 1
-blue = 2
-green = 3
+# red = 1
+# blue = 2
+# green = 3
 
-print((red * 3) + (blue * 5) + (green * 4))
+# print((red * 3) + (blue * 5) + (green * 4))
+
+
 
