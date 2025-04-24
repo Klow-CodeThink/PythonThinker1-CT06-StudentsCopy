b1dd8a1d6558b02118820fa4b6931506f61b4c7f
Klow-CodeThink <keiji.low@computhink.com.sg>
2025-04-24 04:35:25 +0000

    Apr 24, 2025, 4:35 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,21 +1,2 @@
 # print("Hello from lesson 4")
 
-# red = 1
-# blue = 2
-# green = 3
-
-# print((red * 3) + (blue * 5) + (green * 4))
-
-user_name = input("Enter your name: ")
-
-print("Hi, " + user_name + " !")
-
-# print(f"Hi, {user_name}!")
-
-# fav_colour = input("Enter your favourite colour: ")
-# user_age = input("Enter your age: ")
-
-
-# print(fav_colour)
-# print(user_age)
-
