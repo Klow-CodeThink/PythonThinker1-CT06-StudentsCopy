1b3afd0ad409c5091a30dde6b49891d276c764e1
Klow-CodeThink <keiji.low@computhink.com.sg>
2025-04-25 06:54:35 +0000

    Apr 25, 2025, 6:54 AM

---
+++ CT06_10/lesson10_practice.py
@@ -0,0 +1,155 @@
+"""
+# Task 1
+
+num1 = int(input("Enter a number: "))
+
+if num1 > 0:
+    print(f"{num1} is positive.")
+else:
+    print(f"{num1} is negative.")
+"""
+
+"""
+# Task 2
+
+import random
+
+ran_num = random.randint(1, 10)
+print(ran_num)
+
+guess = int(input("Guess the number: "))
+
+if guess == ran_num:
+    print("Congratulations! You did it!")
+else:
+    print("Oops, better luck next time.")
+"""
+
+"""
+# Task 3
+
+pw = "C@deForFun"
+
+login = input("Enter the password: ")
+
+if login == pw:
+    print("Login Successful")
+else:
+    print("Invalid Password")
+"""
+
+"""
+# Task 4
+
+num1 = int(input("Enter a number: "))
+
+if num1 % 2 == 0:
+    print(f"{num1} is an even number.")
+else:
+    print(f"{num1} is an odd number.")
+"""
+
+"""
+# Task 5
+
+user_age = int(input("Enter your age: "))
+
+if user_age < 13:
+    print("Child")
+elif user_age >= 13 and user_age <= 19:
+    print("Teen")
+else:
+    print("Adult")
+"""
+
+"""
+# Task 6
+
+temp = int(input("Enter the temperature: "))
+
+if temp < 20:
+    print("Consider reading indoors.")
+elif temp >= 20 and temp <= 30:
+    print("It is a great day for cycling.")
+else:
+    print("How about a swimming session?")
+"""
+
+"""
+# Task 7
+
+import random
+
+ran_num = random.randint(1, 10)
+print(ran_num)
+
+guess = int(input("Guess the number: "))
+
+if ran_num > guess:
+    print("Higher!")
+else:
+    if ran_num < guess:
+        print("Lower!")
+    else:
+        if ran_num == guess:
+            print("You got it!")
+        else:
+            print("Invalid input!")
+"""
+
+"""
+# Task 7 (Modified) / Task 8
+
+import random
+
+ran_num = random.randint(1, 10)
+print(ran_num)
+
+guess = int(input("Guess the number: "))
+
+if ran_num > guess:
+    print("Higher!")
+elif ran_num < guess:
+    print("Lower!")
+elif ran_num == guess:
+    print("You got it!")
+else:
+    print("Invalid input!")
+"""
+
+"""
+# Task 9
+
+user_score = int(input("Enter the score: "))
+
+if user_score >= 90 and user_score <= 100:
+    print("A")
+elif user_score >= 80 and user_score <= 89:
+    print("B")
+elif user_score >= 70 and user_score <= 79:
+    print("C")
+elif user_score >= 60 and user_score <= 69:
+    print("D")
+else:
+    print("F")
+"""
+
+"""
+# Task 10a
+
+pw = "passme"
+
+user_age = int(input("Enter your age: "))
+
+if user_age < 0:
+    print("Age cannot be negative.")
+elif user_age >= 18:
+    print("Eligible to vote.")
+    user_pw = input("Enter the password: ")         # }
+    if user_pw == "passme":                         # } Task 10b
+        print("Welcome, please cast your vote.")    # }
+    else:
+        print("Access denied.")
+else:
+    print("Not eligible to vote.")
+"""
