7a4714c0b9d78e5438217b4bb117caba89e2b131
Klow-CodeThink <keiji.low@computhink.com.sg>
2025-04-25 07:57:35 +0000

    Apr 25, 2025, 7:57 AM

--- CT06_11/lesson11_practice.py
+++ CT06_11/lesson11_practice.py
@@ -118,7 +118,7 @@ enter_pw = input("Enter your password. ")
 if enter_name == user_name and enter_pw == user_pw:
     print("Access Granted")
 elif enter_name != user_name or enter_pw != user_pw:
-    
+    print("Either username or password is incorrect.")
 
 
 
