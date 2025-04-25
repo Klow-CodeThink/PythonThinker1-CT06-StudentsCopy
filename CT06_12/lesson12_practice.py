57c00a4819b9889e66bd51d70f3012b075992cd3
Klow-CodeThink <keiji.low@computhink.com.sg>
2025-04-25 08:28:56 +0000

    Apr 25, 2025, 8:28 AM

--- CT06_12/lesson12_practice.py
+++ CT06_12/lesson12_practice.py
@@ -42,14 +42,14 @@ while True:
     
 
 
-visitors = 0
+# visitors = 0
 
-while True:
-    visitors += 1
-    print("Visitor number:", visitors)
+# while True:
+#     visitors += 1
+#     print("Visitor number:", visitors)
 
-    if visitors == 30:
-        print("Visitor limit of 30 reached. Closing entry.")
-        break
+#     if visitors == 30:
+#         print("Visitor limit of 30 reached. Closing entry.")
+#         break
 
 
