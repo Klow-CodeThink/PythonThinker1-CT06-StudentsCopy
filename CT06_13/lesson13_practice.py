0e8c6c60741879e3e0ffb4dcf3ca45b667d48943
Klow-CodeThink <keiji.low@computhink.com.sg>
2025-04-25 09:59:22 +0000

    Apr 25, 2025, 9:59 AM

--- CT06_13/lesson13_practice.py
+++ CT06_13/lesson13_practice.py
@@ -5,3 +5,9 @@
 shoplist = []
 
 shoplist.append("apples")
+shoplist.append("bread")
+shoplist.append("apples")
+shoplist.append("apples")
+shoplist.append("apples")
+shoplist.append("apples")
+shoplist.append("apples")
