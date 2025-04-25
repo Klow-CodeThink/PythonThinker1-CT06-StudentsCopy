29d05e11d9052be556ae8729df17a1666f272e52
Klow-CodeThink <keiji.low@computhink.com.sg>
2025-04-25 08:46:19 +0000

    Apr 25, 2025, 8:46 AM

--- CT06_12/lesson12_practice.py
+++ CT06_12/lesson12_practice.py
@@ -54,22 +54,6 @@ while True:
 
 print(f"You have ordered: {order}")
 
-    
-        
-
-
-# order = ""
-
-# while True:
-#     plc_order = input("What would you like to order (type 'end' to finish): ").lower()
-
-#     if plc_order == "end":
-#         break
-
-#     order += plc_order + ", "  # Add the item to the order string
-
-# print("Your final order is:", order)
-    
 
 
 
